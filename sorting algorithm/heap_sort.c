ea4c9dc6b1023c3faf4a947d494e9cdc4ba4a720
Ryan <2272302072@qq.com>
2017-02-17 11:59:32 +0800

    fix heap_sort

--- sorting algorithm/heap_sort.c
+++ sorting algorithm/heap_sort.c
@@ -13,42 +13,24 @@ void swap(int *num, int i, int j){
 
 //虽然堆的左右孩子间没有大小关系，但是为了保证在下沉过程中一次性把沿路的点位置固定，所以
 //同时比较当前结点和左右两个孩子的数值，父节点和两个孩子中较小的点交换
+//n个数字，第i号节点
+//从第0号节点开始排序，i号节点的左孩子是i*2+1，右孩子是i*2+2
+//从第1号节点开始排序，i号节点的左孩子是i*2，右孩子是i*2+1
 void siftdown(int *num, int n, int i){
 	int t;
 	//有孩子，即最少有左孩子
-	while(i*2<n-1){
-		//因为0乘以任何数字都是0，所以当i等于0的时候，无法通过2*i判断父子关系，需要特别判断
-		if(i==0){
-			if(num[i]>num[i+1]){
-				t = i+1;
-			}
-			else{
-				t = i;
-			}
-			if(num[t]>num[i+2]){
-				t = i+2;
-			}
-			//如果父节点已经没法继续下沉，所以已经调整到位,跳出循环
-			if(t==i){
-				break;
-			}
-			else{
-				swap(num, i, t);
-				i = t;
-			}
-			continue;
-		}
+	while(i*2+1<n){
 		//相当于一次需要选出当前节点和两个子节点中的较小值
-		if(num[i]>num[2*i]){
-			t = i*2;
+		if(num[i]>num[2*i+1]){
+			t = i*2+1;
 		}
 		else{
 			t = i;
 		}
 		//如果有右孩子
-		if(i*2+1<n-1){
-			if(num[t]>num[i*2+1]){
-				t = i*2+1;
+		if(i*2+2<n){
+			if(num[t]>num[i*2+2]){
+				t = i*2+2;
 			}
 		}
 		//如果父节点已经没法继续下沉，所以已经调整到位,跳出循环
@@ -62,12 +44,12 @@ void siftdown(int *num, int n, int i){
 	}
 }
 
+//堆顶对应的数组编号是0,从1号开始就直接除以2即可
 void siftup(int *num, int i){
-	//堆顶对应的数组编号是0
 	while(i > 0){
-		if(num[i] < num[i/2]){
-			swap(num, i, i/2);
-			i = i/2;
+		if(num[i] < num[(i-1)/2]){
+			swap(num, i, (i-1)/2);
+			i = (i-1)/2;
 		}
 		else
 			break;
@@ -78,11 +60,16 @@ void siftup(int *num, int i){
 //建立长度为n的最小堆
 void create_heap(int *num, int n){
 	int i;
-	for (i = (n-1)/2; i > 0; i--)
+	//由于涉及到减法，所以需要保证减法之后的结果非负
+	i = (n-1-1)/2;
+	if(i<0)
+		i = 0;
+	for (; i >= 0; i--)
 	{
 		siftdown(num, n, i);
 	}
 }
+
 //对长度为n的数组进行排序
 void heap_sort(int *num, int n){
 	int i;
@@ -109,7 +96,7 @@ int main()
     }
     //初始化建立堆
 	create_heap(num, n);
-	printf("222222222\n");
+
 	heap_sort(num, n);
     
     //for test
